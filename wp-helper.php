b43f8241055c4a8674a9619458005910bbb17868
Felix Lugo <felixdlugo@gmail.com>
2021-03-31 13:56:05 -0400

    Create wp-helper.php
    Helper plugging for avoid displaying permission notices.

---
+++ wp-helper.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Plugin Name:Helper plugging for WP-Rocket
+ * Plugin URI: https://github.com/dexterneutron
+ * Description: Helper for avoiding missing permission notices.
+ * Version: 1.0
+ * Author: Felix Lugo
+ * Author URI: https://github.com/dexterneutron
+ */
+add_action('admin_notices','remove_notices',1);
+function remove_notices(){
+	remove_action('admin_notices', 'rocket_warning_htaccess_permissions');
+	do_hard_unregister_object_callback( 'admin_notices', 10, 'notice_advanced_cache_permissions');
+}
+ function do_hard_unregister_object_callback( $event_name, $priority, $method_name ) {
+	$callback_function = get_object_callback_unique_id_from_registry( $event_name, $priority, $method_name );
+	if ( ! $callback_function ) {
+		return false;
+	}
+	remove_filter( $event_name, $callback_function, $priority );
+}
+function get_object_callback_unique_id_from_registry( $event_name, $priority, $method_name ) {
+	global $wp_filter;
+	if ( ! isset( $wp_filter[ $event_name ][ $priority ] ) ) {
+		return false;
+	}
+	foreach( $wp_filter[ $event_name ][ $priority ] as $callback_function => $registration ) {
+		if ( strpos( $callback_function, $method_name, 32) !== false) {
+			return $callback_function;
+		}
+	}
+	return false;
+}
